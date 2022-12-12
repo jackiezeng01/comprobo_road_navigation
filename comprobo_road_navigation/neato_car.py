ff8df12fb343a4b949e2f7e011238d09484a7775
Melody Chiu <cchiu@olin.edu>
2022-12-11 21:57:52 -0500

    integrate apriltag detection into neatocar

--- comprobo_road_navigation/neato_car.py
+++ comprobo_road_navigation/neato_car.py
@@ -15,6 +15,8 @@ import numpy as np
 from geometry_msgs.msg import Twist, Vector3, Quaternion
 # from comprobo_road_navigation.shape_classification import ShapeClassifier
 from comprobo_road_navigation.obstacle_avoidance import ObstacleAvoidance
+from comprobo_road_navigation.path_planning import PathPlanning
+from comprobo_road_navigation.apriltag_detector import AprilTagDetector
 from comprobo_road_navigation.helper_functions import Point, Line, HoughLineDetection, euler_from_quaternion, undistort_img
 from comprobo_road_navigation.lane_following import Lane_Detector
 
@@ -27,6 +29,7 @@ class NeatoCar(Node):
     def __init__(self, image_topic):
         """ Initialize the ball tracker """
         super().__init__('ball_tracker')
+        self.raw_cv_image = None
         self.cv_image = None                        # the latest image from the camera
         self.bridge = CvBridge()                    # used to convert ROS messages to OpenCV
         self.sub_image = self.create_subscription(Image, image_topic, self.process_image, 10)
@@ -44,11 +47,18 @@ class NeatoCar(Node):
         self.position = None
         self.rotation_speed = 0.3
         self.linear_speed = 0.05
-        # self.shape_classifier = ShapeClassifier()
+        start_node = (3, 5)
+        end_node = (0, 2)
+        self.pathplanner = PathPlanning(start_node, end_node)
+        self.instructions = self.pathplanner.generate_instructions()
+        print(self.instructions)
         self.obstacle_avoidance = ObstacleAvoidance(self.pub)
+        self.apriltag_detector = AprilTagDetector()
         self.lane_detector = Lane_Detector()
         self.velocity = None
         self.turning_flag = False
+        self.drive_straight = True
+        self.turn = False
         # self.velocity = Twist()
         thread = Thread(target=self.loop_wrapper)
         thread.start()
@@ -62,6 +72,7 @@ class NeatoCar(Node):
             called cv_image for subsequent processing """
         cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
         self.cv_image = undistort_img(cv_image)
+        self.raw_cv_image = undistort_img(cv_image)
 
 
     def process_laserscan(self, msg):
@@ -71,11 +82,22 @@ class NeatoCar(Node):
         """ This function takes care of calling the run_loop function repeatedly.
             We are using a separate thread to run the loop_wrapper to work around
             issues with single threaded executors in ROS2 """
-        while True:
+        while True and self.instructions != []:
             if self.orientation and self.position:
                 self.velocity = self.obstacle_avoidance.obstacle_behaviour(self.ranges, self.cv_image, self.orientation, self.position)
-                if self.velocity is None:
-                    self.velocity, self.cv_image = self.lane_detector.run_lane_detector(self.cv_image, self.linear_speed, self.rotation_speed, self.orientation, self.position)
+                if self.velocity is None and self.turning_flag is False:
+                    # self.velocity, self.cv_image = self.lane_detector.run_lane_detector(self.cv_image, self.linear_speed, self.rotation_speed, self.orientation, self.position)
+                    self.velocity = Twist()
+                    self.velocity.linear = Vector3(x=0.1, y=0.0, z=0.0)
+                    self.velocity.angular = Vector3(x=0.0, y=0.0, z=0.0)
+                    instruction = self.instructions[0]
+                    print("instruction:", instruction)
+                    reached, self.cv_image = self.apriltag_detector.run_apriltag_detector(self.cv_image, self.raw_cv_image, instruction)
+                    print("reached: ", reached)
+                    if reached == 1:
+                        self.instructions.pop(0)
+                        self.turning_flag = True
+                        self.turning_behaviour(instruction[1])
                 if self.velocity is not None:
                     self.pub.publish(self.velocity)
                     
@@ -92,6 +114,49 @@ class NeatoCar(Node):
             return Vector3(x=0.0, y=0.0, z=0.0)
         else: 
             return Vector3(x=0.0, y=0.0, z=self.rotation_speed)
+    
+    def turning(self, direction):
+        if self.drive_straight:
+            if math.dist([self.start_position.x, self.start_position.y], [self.position.x, self.position.y]) > 0.4:
+                print("her1")
+                self.drive_straight = False
+                self.turn = True
+                self.start_orientation = None
+                self.velocity.linear = Vector3(x=0.0, y=0.0, z=0.0)
+                self.velocity.angular = Vector3(x=0.0, y=0.0, z=0.0)
+                return self.velocity
+            else:
+                print("her2")
+                self.velocity.linear = Vector3(x=self.linear_speed, y=0.0, z=0.0)
+                self.velocity.angular = Vector3(x=0.0, y=0.0, z=0.0)
+                return self.velocity
+        if self.turn:
+            if abs(self.start_orientation.z - self.orientation.z) >= math.pi/2 - 0.1:
+                print("her3")
+                self.drive_straight = True
+                self.turn = False
+                self.start_orientation = None
+                self.turning_flag = False
+                self.velocity.linear = Vector3(x=0.0, y=0.0, z=0.0)
+                self.velocity.angular = Vector3(x=0.0, y=0.0, z=0.0)
+                return self.velocity 
+            else:  
+                print("her4")               
+                self.velocity.linear = Vector3(x=0.0, y=0.0, z=0.0)
+                if direction == "right":
+                    self.velocity.angular = Vector3(x=0.0, y=0.0, z= -abs(self.rotation_speed))
+                if direction == "left":
+                    self.velocity.angular = Vector3(x=0.0, y=0.0, z= abs(self.rotation_speed))
+                return self.velocity
+
+    def turning_behaviour(self, direction):
+        if self.turning_flag:
+            self.velocity = Twist()
+            if self.start_orientation is None:
+                print("resetting")
+                self.start_orientation = self.orientation
+                self.start_position = self.position 
+            self.velocity = self.turning(direction)
 
     def run_loop(self):
         # NOTE: only do cv2.imshow and cv2.waitKey in this function 
@@ -104,7 +169,7 @@ class NeatoCar(Node):
         #     self.velocity.linear = Vector3(x=0.0, y=0.0, z=0.0)
         #     self.velocity.angular = self.turn_ninety_deg()
         #     print("vel",self.velocity)
-       if not self.cv_image is None:
+        if not self.cv_image is None:
             cv2.imshow('video_window', self.cv_image)
             cv2.waitKey(5)
             # print(self.calibrate_mask)
